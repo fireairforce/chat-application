8dfe51a149edb091efe848dc5988126c491330c1
zoomdong <1344492820@qq.com>
2019-02-20 23:05:28 +0800

    对消息列表的最新消息进行了排序

--- README.md
+++ README.md
@@ -10,3 +10,13 @@
 - 完成聊天功能的实现，建议开两种不同的浏览器(例如firefox和chrome)进行尬聊
 - 聊天窗口里面有个antd-mobile Card组件的显示问题，利用官方给的办法解决
 - // console.log(Object.values({ name:'zoomdong',age:18 }));把数组拼接为一个对象
+- js里面的排序
+```js
+console.log([6,4,5,9,7,8].sort((a,b)=>{
+    return a-b; //根据从小到大的顺序排序
+}));
+```
+
+1. eslint
+2. react16的错误机制
+3. react性能优化
--- src/component/msg/msg.js
+++ src/component/msg/msg.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { List } from 'antd-mobile'
+import { List, Badge } from 'antd-mobile'
 class Msg extends React.Component{
     getLast = (arr) => {
         return arr[arr.length - 1];
@@ -21,33 +21,42 @@ class Msg extends React.Component{
             msgGroup[v.chatid] = msgGroup[v.chatid] || []
             msgGroup[v.chatid].push(v);
         })     
-        // console.log(msgGroup); // 这个里面存储所有的chat_id带上他们的聊天信息(chatmsg)
+        // console.log(Object.values(msgGroup)); // 这个里面存储所有的chat_id带上他们的聊天信息(chatmsg)
         // console.log(Object.values({ name:'zoomdong',age:18 }));
-        const chatList = Object.values(msgGroup); // 把msgGroup拼接成一个数组
+       
+        const chatList = Object.values(msgGroup).sort((a,b)=>{
+            const a_last = this.getLast(a).create_time;
+            const b_last = this.getLast(b).create_time;
+            return b_last - a_last //根据在后端创建的一个时间戳来进行对信息的一个排序
+        }); 
+        // 把msgGroup(对象)拼接成一个数组并且进行排序
         // console.log(chatList);
+
         return(
-            <div>
-             
+            <div>            
                    {
-                    //    chatList.filter().map()
-                    // v[0].from
                   　　　chatList.map(v=>{
                         // console.log(v);
                         const lastItem = this.getLast(v);
                         // console.log(lastItem);
                         const targetid = lastItem.from ==userid?lastItem.to:lastItem.from; //这个地方永远存储的是和你聊天的那个人的名字
+                        const unreadNum = v.filter(v=>!v.read&&v.to===userid).length;
                         if(!users[userid]){
                             return null;
                         }
-                        const name = users[targetid]?users[targetid].name:null;
-                        const avatar = users[targetid]?users[targetid].avatar:null;
+                        const name = users[targetid].name;
+                        const avatar = users[targetid].avatar;
                     return( 
                         <List 
                           key={lastItem._id} // _id这里是不会出现重复的
                         >
                         <Item 
-                          
+                          extra = {<Badge text={unreadNum}></Badge>}
                           thumb={require(`../img/${avatar}.png`)}
+                          arrow='horizontal'
+                          onClick={()=>{
+                             this.props.history.push(`/chat/${targetid}`)
+                          }}
                         >
                             {lastItem.content}
                           <Brief>{name}</Brief>
