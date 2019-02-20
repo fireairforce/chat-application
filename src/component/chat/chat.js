a0d12445cea97bfbe6ed3da26ae762d4e4459c0a
zoomdong <1344492820@qq.com>
2019-02-20 20:56:39 +0800

    引入emoji符号表情

--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List,InputItem, NavBar,Icon} from 'antd-mobile';
+import { List,InputItem, NavBar,Icon,Grid } from 'antd-mobile';
 // import io from 'socket.io-client';
 import { connect } from 'react-redux';
 import { getMsgList,sendMsg ,recvMsg} from '../../redux/chat.redux';
@@ -15,8 +15,15 @@ class Chat extends React.Component{
         if(!this.props.state.chat.chatmsg.length){
             this.props.getMsgList();
             this.props.recvMsg();
-        }   
+        }
+        
     }    
+    fixCarousel=()=>{
+        console.log('1');
+        setTimeout(function(){
+            window.dispatchEvent(new Event('resize'))
+        },0)
+    }
     handleSubmit = () =>{
         // socket.emit('sendmsg',{text:this.state.text}); // 利用socket给后端发送数据
         // this.setState({ text:'' })
@@ -25,20 +32,25 @@ class Chat extends React.Component{
         const msg = this.state.text;
         this.props.sendMsg({from,to,msg});
         //发送完成之后，我们直接把state设置为空
-        this.setState({ text:'' })
+        this.setState({ text:'',showEmoji:false })
     }
     render(){
+        const emoji = '😀 😃 😄 😁 😆 😅 😂 😊 😇 🙂 🙃 😉 😌 😍 😘 😗 😙 😚 😋 😜 😝 😛 🤑 🤗 🤓 😎 😏 😒 😞 😔 😟 😕 🙁 😣 😖 😫 😩 😤 😠 😡 😶 😐 😑 😯 😦 😧 😮 😲 😵 😳 😱 😨 😰 😢 😥 😭 😓 😪 😴 🙄 🤔 😬 🤐 😷 🤒 🤕 😈 👿 👹 👺 💩 👻 💀 ☠️ 👽 👾 🤖 🎃 😺 😸 😹 😻 😼 😽 🙀 😿 😾 👐 🙌 👏 🙏 👍 👎 👊 ✊ 🤘 👌 👈 👉 👆 👇 ✋  🖐 🖖 👋  💪 🖕 ✍️  💅 🖖 💄 💋 👄 👅 👂 👃 👁 👀 '
+                                        .split(' ') //把他们通过空格分割成一个数组
+                                        .filter(v=>v) // 过滤空格
+                                        .map(v=>({text:v})) 
         const userid = this.props.match.params.user;
         const Item = List.Item;
         const users = this.props.state.chat.users;
+        const { showEmoji } = this.state;
         // console.log(users);
-        console.log(this.props);
+        // console.log(this.props);
         if(!users[userid]){
             return null
         }
         const chatid = getChatId(userid,this.props.state.user._id);
         const chatmsgs = this.props.state.chat.chatmsg.filter(v=>v.chatid===chatid); //进行一些数据的过滤
-
+  
         return(
             <div id="chat-page">
                 <NavBar
@@ -81,11 +93,36 @@ class Chat extends React.Component{
                       onChange={v=>{
                           this.setState({ text:v })
                       }}
-                      extra={<span onClick={this.handleSubmit}>发送</span>}
+                      extra={
+                      <div>
+                          <span
+                            style={{marginRight:15}}
+                            onClick={()=>{
+                                this.setState({showEmoji:!this.state.showEmoji})
+                                this.fixCarousel();// 这个函数是官方用来修复Grid的框框不能显示的问题的，点击的时候执行一下这个函数
+                            }}       
+                          >😀</span>
+                         <span onClick={this.handleSubmit}>发送</span>
+                      </div>                    
+                    }
                      >
                      信息        
                     </InputItem>
                 </List>
+                 {showEmoji?
+                     (<Grid data={emoji}
+                     columnNum = {9}
+                     carouselMaxRow={4}
+                     isCarousel={true}
+                     onClick={el=>{
+                        //  console.log(el);//el 是我们选择的那个表情，把它加到输入信息框就可以了
+                        this.setState({
+                            text:this.state.text + el.text
+                        })
+                        
+                     }}
+                     />):null
+                }
                 </div>
             </div>
         )
@@ -98,3 +135,4 @@ const actionCreators = { getMsgList ,sendMsg,recvMsg }
 
 Chat = connect(mapStateToProps,actionCreators)(Chat)
 export default Chat;
+
